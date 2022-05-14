02134a6a189fb8a775aedf289ec458512ca1d7d6
alex <aoklyunin@gmail.com>
2022-05-14 13:42:09 +0300

    static styles beginning

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from djangoProject import settings
+from tasks import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  # ex: /polls/
+                  path('', views.index, name='index'),
+                  path('admin/', admin.site.urls),
+              ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,17 @@
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader
+
+from tasks.models import Task
+
+
+def index(request):
+    latest_question_list = Task.objects.order_by('author')
+    template = loader.get_template('tasks/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
